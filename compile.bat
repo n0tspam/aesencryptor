563f784fc1b02d69ee1c7fb0b3263a6c2ba330b0
unknown <wg2159@nyu.edu>
2022-10-26 03:23:18 -0400

    'updated'

---
+++ compile.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.33.31629\bin\Hostx64\x64\cl.exe" /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tcimplant.cpp /link /OUT:implant.exe /SUBSYSTEM:CONSOLE /MACHINE:x64
