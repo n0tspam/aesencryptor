5ae321668319ae3ad5e2dd39b32f0afe95bfdf53
unknown <wg2159@nyu.edu>
2022-10-26 03:22:27 -0400

    fixed untracked files

--- compile.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.33.31629\bin\Hostx64\x64\cl.exe" /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tcimplant.cpp /link /OUT:implant.exe /SUBSYSTEM:CONSOLE /MACHINE:x64
